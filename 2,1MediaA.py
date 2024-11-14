5c80c29bfb2cb7ddcbca0a187fcee93bdbc0e39a
Arthur-Epic <arthurtutu464@gmail.com>
2024-11-13 21:05:52 -0300

    robotprince

--- 2,1MediaA.py
+++ 2,1MediaA.py
@@ -1,10 +1,12 @@
-a = int(input("digite sua primeira nota: "))
-b = int(input("digite sua segunda nota: "))
-c = int(input("digite sua terceira nota: "))
-d = int(input("digite sua quarta nota: "))
+print("MEDIA DE ALGUMA COISA AI")
+
+a = float(input("digite sua primeira nota: "))
+b = float(input("digite sua segunda nota: "))
+c = float(input("digite sua terceira nota: "))
+d = float(input("digite sua quarta nota: "))
 
 soma = a+b+c+d
 
 media = soma/4
 
-print(media)
+print("\n ta ai sua media:", media)
